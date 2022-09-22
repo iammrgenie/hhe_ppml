c50c61f949f0e0b73c0218f3678906f7b667620a
Eugene <eugene.frimpong@tuni.fi>
2022-09-22 15:52:35 +0300

    hhe

---
+++ build/util/keccak/CMakeFiles/keccak.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mrgenie/Projects/hhe_ppml/util/keccak/KeccakHash.c" "util/keccak/CMakeFiles/keccak.dir/KeccakHash.c.o" "gcc" "util/keccak/CMakeFiles/keccak.dir/KeccakHash.c.o.d"
+  "/home/mrgenie/Projects/hhe_ppml/util/keccak/KeccakSpongeWidth1600.c" "util/keccak/CMakeFiles/keccak.dir/KeccakSpongeWidth1600.c.o" "gcc" "util/keccak/CMakeFiles/keccak.dir/KeccakSpongeWidth1600.c.o.d"
+  "/home/mrgenie/Projects/hhe_ppml/util/keccak/avx2/KeccakP-1600-AVX2.s" "util/keccak/CMakeFiles/keccak.dir/avx2/KeccakP-1600-AVX2.s.o" "gcc" "util/keccak/CMakeFiles/keccak.dir/avx2/KeccakP-1600-AVX2.s.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/util/keccak/CMakeFiles/keccak.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keccak.dir/KeccakHash.c.o"
+  "CMakeFiles/keccak.dir/KeccakHash.c.o.d"
+  "CMakeFiles/keccak.dir/KeccakSpongeWidth1600.c.o"
+  "CMakeFiles/keccak.dir/KeccakSpongeWidth1600.c.o.d"
+  "CMakeFiles/keccak.dir/avx2/KeccakP-1600-AVX2.s.o"
+  "CMakeFiles/keccak.dir/avx2/KeccakP-1600-AVX2.s.o.d"
+  "libkeccak.a"
+  "libkeccak.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/keccak.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
