b10f0bb4a1564095bc230b2f30d8d0d883419e9a
khoaguin <dkn.work@protonmail.com>
2022-10-05 16:01:47 +0300

    update server.cpp to be same as upstream/main

--- 3-party-HHE/user.cpp
+++ 3-party-HHE/user.cpp
@@ -11,7 +11,7 @@
 #include "../src/sealhelper.h"
 
 #define NUM_RUN 50 // number of runs to get the average measurements
-#define NUM_VEC 100 // number of vectors the user have
+#define NUM_VEC 10 // number of vectors the user have
 static const bool USE_BATCH = true;
 
 struct UserData {
